dc6f30141620c3bae3719077a5e8b6f72c124c97
Vimal Sonara <vvsonara95@gmail.com>
2024-07-04 09:04:32 +0530

    chore: apollo-graphql react configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const client = new ApolloClient({
+  uri: "http://localhost:8080/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
