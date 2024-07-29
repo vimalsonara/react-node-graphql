5d3b87c2bf5c9847ee6b50985ab84db8b54477ad
Vimal Sonara <vvsonara95@gmail.com>
2024-07-29 08:09:23 +0530

    chore: tailwind & shadcn config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import "./index.css";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/graphql",
